ad368b8852a67a4f4befc54878dc1b778bec4e15
MAAS-Edtech <114066097+Thinkr-Trung@users.noreply.github.com>
2022-11-09 21:43:51 +0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Work_Sans } from "@next/font/google";
+import { ChakraProvider } from "@chakra-ui/react";
+import Head from "next/head";
+const workSans = Work_Sans();
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <ChakraProvider className={workSans.className}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/api/login.js
@@ -0,0 +1,16 @@
+import axios from "axios"
+
+export default async function handler(req, res) {
+    console.log(req.body)
+    let body = req.body
+    await axios.post("https://athena.skymavis.com/v1/rpc/auth/login", body).then((data) => {
+        res.status(200).json(data.data)
+        return
+    })
+    .catch((ex)=>{
+        console.log(ex)
+        res.status(500).json("Error")
+        return
+    })
+  }
+  
